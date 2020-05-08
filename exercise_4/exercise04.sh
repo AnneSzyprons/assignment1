b44373d265500e089139753fc79f88432c33580e
AnneSzyprons <63041907+AnneSzyprons@users.noreply.github.com>
2020-05-08 16:13:22 +0200

    update exercise04

--- exercise_4/exercise04.sh
+++ exercise_4/exercise04.sh
@@ -1,1 +1,7 @@
-grep -ohE [0-9]{4}-[0-9]{3}[X|0-9] 2014-01_JA.tsv 2014-01-31_JA-africa.tsv 2014-01-31_JA-america.tsv 2014-02-02_JA-britain.tsv
+#!/bin/bash
+grep -iwcE 'china|chinese' *.tsv
+cut -f 9 *.tsv | grep -iwcE 'china|chinese'
+cut -f 6 *.tsv | grep -E '[0-9]{4}-[0-9]{3}[X|0-9]' | wc -l
+cut -f2 *.tsv | sed '/^$/d'
+cut -f2 *.tsv | sed '/^$/d' | sort | uniq
+cut -f 6 *.tsv | grep -ohE '[0-9]{4}-[0-9]{3}[X|0-9]' | sort -n | uniq
